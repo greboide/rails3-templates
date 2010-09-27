121b2c29337270f00c1c8c26c4ad060f0d57c6a7
Glauber Prado <smade4@gmail.com>
2010-09-26 22:18:01 -0300

    rails 3

--- template-gems.rb
+++ template-gems.rb
@@ -24,7 +24,7 @@ file 'Gemfile', <<-FILE
 source 'http://rubygems.org'
 source 'http://gems.github.com'
 
-gem 'rails', '3.0.0.beta4'
+gem 'rails'
 gem 'sqlite3-ruby', :require => 'sqlite3'
 
 # Use unicorn as the web server
